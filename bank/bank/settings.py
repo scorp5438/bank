b075a81d0f8fb0aa77d4a41300625ce493b96f25
Punisher <alex_77_90@mail.ru>
2025-02-02 23:02:01 +0500

    add .env.template

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tw=r5^8=ob934lb0)a#uw43u7!@mafrzm1ngl82cgp6qjo4_-q'
+SECRET_KEY = getenv(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-tw=r5^8=ob934lb0)a#uw43u7!@mafrzm1ngl82cgp6qjo4_-q'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv(
+    'DJANGO_DEBUG',
+    True
+)
 
 ALLOWED_HOSTS = []
 
