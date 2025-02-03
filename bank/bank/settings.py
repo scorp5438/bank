231d2dfc88f5be433f15afeea5c0cc22896afc3b
alex <alex_77_90@mail.ru>
2025-02-04 00:12:27 +0500

    update yml

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -139,5 +139,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
