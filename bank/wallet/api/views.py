6f06869b60e91c238a876dafd643d7c54f97cef6
alex <alex_77_90@mail.ru>
2025-02-05 20:47:35 +0500

    Upgrade views

--- bank/wallet/api/views.py
+++ bank/wallet/api/views.py
@@ -25,10 +25,6 @@ class UpdateWalletApiView(viewsets.ViewSet):
     def update(self, request, pk=None):
         operation_type = request.data.get('operationType')
         amount = request.data.get('amount')
-        # wallet = cache.get(f'wallet_{pk}')
-        # if not wallet:
-        #     wallet = self.get_wallet_with_retries(pk)
-        #     cache.set(f'wallet_{pk}', wallet)
 
         try:
             amount = Decimal(amount)
@@ -38,21 +34,13 @@ class UpdateWalletApiView(viewsets.ViewSet):
         if amount < 0:
             return Response({"error": "Сумма должна быть положительной."}, status=400)
 
-        # if wallet is None:
-        #     return Response({"error": "Кошелек не найден."}, status=404)
-
         with transaction.atomic():
             wallet = self.get_wallet_with_retries(pk)
             if wallet is None:
                 return Response({"error": "Кошелек не найден."}, status=404)
             if operation_type == 'DEPOSIT':
-                # wallet.balance = F('balance') + amount
                 Wallet.objects.filter(pk=wallet.pk).update(balance=F('balance') + amount)
             elif operation_type == 'WITHDRAW':
-                # if wallet.balance >= amount:
-                #     wallet.balance = F('balance') - amount
-                #
-                # else:
                 temp = Wallet.objects.filter(pk=wallet.pk, balance__gte=amount).update(balance=F('balance') - amount)
                 if not temp:
                     return Response({"error": "На балансе не достаточно средств."}, status=400)
@@ -61,8 +49,6 @@ class UpdateWalletApiView(viewsets.ViewSet):
                 return Response({"error": "Некорректные данные: operationType должен быть 'DEPOSIT' или 'WITHDRAW'."},
                                 status=400)
 
-            # wallet.save(update_fields=['balance',])
-            # wallet.refresh_from_db()
             wallet = Wallet.objects.get(pk=wallet.pk)
             cache.set(f'wallet_{pk}', wallet)
         return Response({"message": f"баланс кошелька {pk} успешно изменен. Текущий баланс {wallet.balance}"},
@@ -72,7 +58,7 @@ class UpdateWalletApiView(viewsets.ViewSet):
     def get_wallet_with_retries(self, wallet_id, retries=3, delay=1):
         for attempt in range(retries):
             try:
-                return Wallet.objects.select_for_update().get(id=wallet_id) # of=('self',), skip_locked=True
+                return Wallet.objects.select_for_update().get(id=wallet_id)
             except Wallet.DoesNotExist:
                 return None
             except OperationalError:
